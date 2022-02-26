b8684cec5a0b39b2e9f4c1f85737197615ce8ccc
Ryan Hausen <ryan.hausen@gmail.com>
2022-02-25 17:07:58 -0800

    wip model

--- plot_helpers.py
+++ plot_helpers.py
@@ -4,12 +4,12 @@ import numpy as np
 
 
 # https://matplotlib.org/stable/gallery/lines_bars_and_markers/scatter_hist.html
-def scatter_hist(x, y, ax, ax_histx, ax_histy):
+def scatter_hist(dist, x, y, ax, ax_histx, ax_histy):
     # no labels
     ax_histx.tick_params(axis="x", labelbottom=False)
     ax_histy.tick_params(axis="y", labelleft=False)
 
-    # the scatter plot:
+    
 
     # now determine nice limits by hand:
     binwidth = 0.25
@@ -18,12 +18,10 @@ def scatter_hist(x, y, ax, ax_histx, ax_histy):
 
     bins = np.arange(-lim, lim + binwidth, binwidth)
     
-    vals = ax.hist2d(x, y, bins=bins, cmap="Blues", density=True)
+    vals = ax.scatter(x, y, c="tab:blue", marker="o", alpha=0.5)
     ax.annotate("Joint", (0.02, 0.97), xycoords="axes fraction")
     ax.set_ylabel("$Y$")
     ax.set_xlabel("$X$")
-    cbar = plt.colorbar(vals[-1])
-    cbar.ax.set_ylabel("P($X$,$Y$)")
     
     ax_histx.hist(x, bins=bins, density=True)
     ax_histx.annotate("Marginal X", (0.02, 0.90), xycoords="axes fraction")
@@ -31,7 +29,14 @@ def scatter_hist(x, y, ax, ax_histx, ax_histy):
     ax_histy.hist(y, bins=bins, orientation='horizontal', density=True)
     ax_histy.annotate("Marginal Y", (0.05, 0.97), xycoords="axes fraction")
 
-def hist2d_marginals(x, y):
+    #y_min, y_max = ax.get_ylim()
+    #x_min, x_max = ax.get_xlim()
+    #xs, ys = np.meshgrid(np.linspace(x_min, x_max, num=100), np.linspace(y_min, y_max, num=100))
+    #vals = np.array([[y, x] for y, x in zip(ys.flatten(), xs.flatten())], dtype=np.float32)
+    #zs = dist.prob(vals).numpy().reshape([100, 100])
+    #ax.contour(xs, ys, zs, alpha=0.5, cmap="jet")
+
+def hist2d_marginals(dist, x, y):
     left, width = 0.1, 0.65
     bottom, height = 0.1, 0.65
     spacing = 0.005
@@ -49,7 +54,7 @@ def hist2d_marginals(x, y):
     ax_histy = fig.add_axes(rect_histy, sharey=ax)
 
     # use the previously defined function
-    scatter_hist(x, y, ax, ax_histx, ax_histy)
+    scatter_hist(dist, x, y, ax, ax_histx, ax_histy)
 
     plt.show()
     
